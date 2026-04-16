e8408499685d9316487841052b7c7fd80109b4c0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2026-04-16 13:18:36 +0530

    authentication

--- discord/src/bot.py
+++ discord/src/bot.py
@@ -1,4 +1,8 @@
 import discord
+import credentials
+import firebase_admin
+import firebase_admin.credentials
+import firebase_admin.firestore
 
 from discord.ext import commands
 
@@ -11,6 +15,29 @@ class DiscordBot(commands.Bot):
             help_command=None,
             description="Discord Interface for https://doomsday-clock-for-archie.vercel.app/",
         )
+
+        (
+            firebase_admin.initialize_app(
+                credential=firebase_admin.credentials.Certificate(
+                    {
+                        "type": credentials.FIREBASE_TYPE,
+                        "project_id": credentials.FIREBASE_PROJECT_ID,
+                        "private_key_id": credentials.FIREBASE_PRIVATE_KEY_ID,
+                        "private_key": credentials.FIREBASE_PRIVATE_KEY,
+                        "client_email": credentials.FIREBASE_CLIENT_EMAIL,
+                        "client_id": credentials.FIREBASE_CLIENT_ID,
+                        "auth_uri": credentials.FIREBASE_AUTH_URI,
+                        "token_uri": credentials.FIREBASE_TOKEN_URI,
+                        "auth_provider_x509_cert_url": credentials.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+                        "client_x509_cert_url": credentials.FIREBASE_CLIENT_X509_CERT_URL,
+                    }
+                )
+            )
+            if not len(firebase_admin._apps)
+            else firebase_admin.get_app()
+        )
+
+        self.db = firebase_admin.firestore.client()
 
     async def setup_hook(self):
         await self.load_extension("src.commands")
--- discord/src/commands.py
+++ discord/src/commands.py
@@ -1,8 +1,33 @@
 import discord
+import credentials
 
+from typing import Optional
 from discord.ext import commands
 from discord import app_commands
 from src.bot import DiscordBot
+
+
+def authenticate(user: str) -> (bool, str, bool):
+    view_allowed = False
+    name = ""
+    edit_allowed = False
+
+    if user == credentials.BRO_ID:
+        view_allowed = True
+        name = credentials.BRO_NAME
+        edit_allowed = True
+    elif user == credentials.SIS_ID:
+        view_allowed = True
+        name = credentials.SIS_NAME
+        edit_allowed = True
+    elif user in credentials.SPECTATOR_IDS.split(","):
+        view_allowed = True
+        edit_allowed = False
+    else:
+        view_allowed = False
+        edit_allowed = False
+
+    return view_allowed, name, edit_allowed
 
 
 class Commands(commands.Cog):
@@ -13,19 +38,30 @@ class Commands(commands.Cog):
     @app_commands.describe(
         time="Set the Time of the Clock",
         message="Set the Message",
-        name="Authentication Name",
-        password="Authentication Password",
+    )
+    @app_commands.choices(
+        time=[app_commands.Choice(name=str(i), value=i) for i in range(1, 13)]
     )
     async def update_message(
         self,
         inter: discord.Interaction,
         time: int,
         message: str,
-        name: str,
-        password: str,
     ):
-        # await inter.response.send_message(f"{message} - {time} | Test Message")
-        pass
+        view_allowed, name, edit_allowed = authenticate(str(inter.user.id))
+
+        if not view_allowed:
+            return await inter.response.send_message("Hey, Its None of your Business!")
+
+    @app_commands.command(name="list", description="List the message(s)")
+    @app_commands.describe(all="Whether to show the whole List, or only the Latest")
+    async def get_messages(
+        self, inter: discord.Interaction, all: Optional[bool] = None
+    ):
+        view_allowed, name, edit_allowed = authenticate(str(inter.user.id))
+
+        if not view_allowed:
+            return await inter.response.send_message("Hey, Its None of your Business!")
 
 
 async def setup(bot: DiscordBot):
